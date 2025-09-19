31ffbd3479ee1639ac219422e701195803bb7395
wcc-duckie <bhammond@waubonsee.edu>
2025-09-19 07:22:27 -0500

    fixed name in github action

--- src/main.py
+++ src/main.py
@@ -1,1 +1,1 @@
-print("If you installed this `paxel` library, you are probably looking for an internal WCC `paxel-sdk` library.")
+print("If you installed this `paxel-sdk` library, you are probably looking for an internal WCC `paxel-sdk` library.")
